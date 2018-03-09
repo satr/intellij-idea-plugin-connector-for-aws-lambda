e57f7a82e2c644f0a0934801cc5cd3a739d4a096
satr <CFGtgrivt46>
2018-03-09 14:49:03 +0100

    Supported artifacts from Maven projects.

--- sources/src/io/github/satr/idea/plugin/connector/la/entities/ArtifactEntity.java
+++ sources/src/io/github/satr/idea/plugin/connector/la/entities/ArtifactEntity.java
@@ -2,34 +2,43 @@ package io.github.satr.idea.plugin.connector.la.entities;
 // Copyright © 2018, github.com/satr, MIT License
 
 import com.intellij.packaging.artifacts.Artifact;
-import com.intellij.packaging.artifacts.ArtifactType;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ArtifactEntity {
 
     private final String name;
     private final String outputFilePath;
-    private final ArtifactType artifactType;
+    private boolean isMavenized;
 
     public ArtifactEntity(Artifact artifact) {
-        name = artifact.getName();
-        outputFilePath = artifact.getOutputFilePath();
-        artifactType = artifact.getArtifactType();
+        this(artifact.getName(), artifact.getOutputFilePath(), false);
+    }
+
+    public ArtifactEntity(String artifactName, String artifactFilePath, boolean isMavenized) {
+        name = artifactName;
+        this.outputFilePath = artifactFilePath;
+        this.isMavenized = isMavenized;
+    }
+
+    public ArtifactEntity(String artifactName, String artifactPath, String artifactPackage, boolean isMavenized) {
+        this(artifactName, Paths.get(artifactPath, artifactName).toAbsolutePath().toString(), isMavenized);
     }
 
     public String getName() {
         return name;
     }
-
     public String getOutputFilePath() {
         return outputFilePath;
     }
 
-    public ArtifactType getArtifactType() {
-        return artifactType;
-    }
-
     @Override
     public String toString() {
-        return String.format("%s", getName(), getArtifactType().toString());
+        return String.format("%s%s", getName(), getTypeDescription());
+    }
+
+    public String getTypeDescription() {
+        return isMavenized ? " (Maven)" : "";
     }
 }
--- sources/src/io/github/satr/idea/plugin/connector/la/models/ProjectModelImpl.java
+++ sources/src/io/github/satr/idea/plugin/connector/la/models/ProjectModelImpl.java
@@ -6,6 +6,8 @@ import com.intellij.packaging.artifacts.Artifact;
 import com.intellij.packaging.artifacts.ArtifactManager;
 import com.intellij.packaging.artifacts.ArtifactType;
 import io.github.satr.idea.plugin.connector.la.entities.ArtifactEntity;
+import org.jetbrains.idea.maven.project.MavenProject;
+import org.jetbrains.idea.maven.project.MavenProjectsManager;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -14,9 +16,11 @@ import java.util.Collections;
 public class ProjectModelImpl implements ProjectModel {
     private final String JAR_ARTIFACT_TYPE = "jar";
     private final Project project;
+    private final MavenProjectsManager mavenProjectsManager;
 
     public ProjectModelImpl(Project project) {
         this.project = project;
+        mavenProjectsManager = MavenProjectsManager.getInstance(project);
     }
 
     @Override
@@ -24,12 +28,30 @@ public class ProjectModelImpl implements ProjectModel {
         if(project == null) {
             return Collections.emptyList();
         }
+        final ArrayList<ArtifactEntity> artifactEntries = new ArrayList<>();
+        addProjectArtifacts(artifactEntries);
+        addMavenProjectArtifacts(artifactEntries);
+        return artifactEntries;
+    }
+
+    private void addMavenProjectArtifacts(ArrayList<ArtifactEntity> artifactEntries) {
+        if (mavenProjectsManager == null || !mavenProjectsManager.isMavenizedProject()) {
+            return;
+        }
+        for(MavenProject mavenProject : mavenProjectsManager.getProjects()) {
+            if(!JAR_ARTIFACT_TYPE.equals(mavenProject.getPackaging())){
+                continue;
+            }
+            String artifactName = String.format("%s.%s", mavenProject.getFinalName(), mavenProject.getPackaging());
+            artifactEntries.add(new ArtifactEntity(artifactName, mavenProject.getBuildDirectory(), artifactName, true));
+        }
+    }
+
+    private void addProjectArtifacts(ArrayList<ArtifactEntity> artifactEntries) {
         final ArtifactManager artifactManager = ArtifactManager.getInstance(this.project);
         final Collection<? extends Artifact> jarArtifacts = artifactManager.getArtifactsByType(ArtifactType.findById(JAR_ARTIFACT_TYPE));
-        final ArrayList<ArtifactEntity> artifactEntries = new ArrayList<>();
         for(Artifact artifact : jarArtifacts) {
             artifactEntries.add(new ArtifactEntity(artifact));
         }
-        return artifactEntries;
     }
 }
